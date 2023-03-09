e7d7079e5a057488723ca4198697998dd72fc49a
alvaroSantos07 <alvaro.santos.fumcad@despertar.org.br>
2023-03-09 09:55:54 -0300

    criar pasta

---
+++ algoritmo.js
@@ -0,0 +1,15 @@
+let numeroUm = 0;
+let numeroDois = 10;
+let numeroTres = 20;
+
+//diferença entre const e var: "const" têm escopo de bloco, enquanto "var" tem escopo global ou de função.
+//diferença entre let e var: "let" é uma variavel que pega somente os "dados" que estão dentro da chave, e "var" é uma variavel global.
+
+console.log(numeroUm)
+//Console.log tem a função de printar a codificação, informando o resultado!
+
+numeroUm = numeroDois;
+console.log(numeroUm)
+
+numeroUm = numeroTres;
+console.log(numeroUm);
---
+++ let-const-var.js
@@ -0,0 +1,8 @@
+if (true){
+   let a = 10;
+   console.log (a);
+
+}
+
+//console.log(a): Quando o console.log está fora do escopo, a variavel "let" não irá reconehcer pelo fato de não ser uma variavel global
+// Diferença entre let e var: "let" é uma variavel que pega somente os "dados" que estão dentro do escopo, e "var" é uma variavel global. 
